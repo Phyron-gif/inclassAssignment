50dfb44457ae7c19a436ef25f6133516563b185d
Tracy Phyron <tracy.jinor@ashesi.edu.gh>
2025-10-07 11:05:13 +0000

    dontunderstand yet

---
+++ practice.java
@@ -0,0 +1,40 @@
+public class practice{
+    public static void main(String[] args){
+        /*we are writting a program that does linear search that is if we have a target number we 
+        want to find in mind then we search from one index to another until we get the required value.
+        now what do i need to know to solve this?
+        first i need an array of values as input
+        i need a target value
+        my final output should be the target value
+        now what do i need to solve this problem?
+        need to difine an array
+        first i would need to define a target number
+        since we are searching through an array of numbers, we need a loop 
+        and because i know the array numbers i would need a for loop.
+        also i would need a conditional that is to check when the a number in the array matches the targeted number
+        steps:
+        define an interger of arrays
+        define a target value
+        check for index value starting from zero and ending on a value not greater than the lenth of the array with an increment of one each time it runs
+        now check if the first index value is same as target number
+        if same print we found number at the index found
+        then stop execution
+
+
+
+         * 
+         */
+        int[] linearSearch = {1,4,5,6,8,9};
+        int targetValue = 5;
+        int counting = -1;
+        
+        for (int index=0; index < linearSearch.length; index++){
+            if (linearSearch[index] == targetValue){
+                counting = index;
+                System.out.println("we found "+ targetValue+"at index" + index);
+                
+            }
+        }
+        
+    }
+}
